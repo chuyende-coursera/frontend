122786a782f969d55ab1097ff5a91fed7e11f346
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2021-04-08 22:34:47 +0700

    Add container state for dashboard

---
+++ src/containerDashboard/HomeContainer.js
@@ -0,0 +1,6 @@
+import { connect } from "react-redux";
+import Home from "../componentDashboard/Home/Home";
+
+// const mapStateToProps = (state) => state;
+
+export default connect()(Home);
---
+++ src/containerDashboard/inventory/RevenueContainer.js
@@ -0,0 +1,6 @@
+import { connect } from "react-redux";
+import Revenue from "../../componentDashboard/inventory/Revenue";
+
+// const mapStateToProps = (state) => state;
+
+export default connect(null, null)(Revenue);
