e43d17d1d203637ddd87aa1b10ef9dd203ff0886
leminhduc201299 <duc.lm173043@sis.hust.edu.vn>
2021-05-20 09:23:44 +0700

    Fix bub and add reducers for job, add router

---
+++ src/reducers/initialState.js
@@ -0,0 +1,14 @@
+export default {
+  topics: [],
+  courses: [],
+  course: {},
+  session: {},
+  apiCallsInProgress: 0,
+  jobs: [],
+  skills: [],
+  languages: [],
+  weeks: [],
+  week: {},
+  userCourse: [],
+  currentUser: {},
+};
